1d20e7e51b104897781e1fd3f95d069f39422ef7
Jake Nickel <jnicke7@illinois.edu>
2022-05-04 14:22:47 -0500

    More work performed on VGG, now need to implement Gram matrix calc

--- main.py
+++ main.py
@@ -3,19 +3,33 @@
 # Santhosh Bomminani, Jake Nickel
 # CS 445, 2022
 
+import warnings
+warnings.filterwarnings('ignore',category=FutureWarning)
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.preprocessing.image import array_to_img
 
-def init_model():
-	# The layer that contains the "content" understanding of the input images
-	content_layer = "block5_conv2" 
-
+def init_model(layers):
 	full_model = tf.keras.applications.VGG19(include_top=False, weights='imagenet')
 	full_model.trainable = False
-	content_output = full_model.get_layer(content_layer).output
-	specific_model = keras.Model(full_model.input, content_output)
+	outputs = [full_model.get_layer(layer_name).output for layer_name in layers]
+	specific_model = keras.Model(full_model.input, outputs)
 	return specific_model
 
-
+def gram(input_tensor):
+	pass		
 	
+img = (img_to_array(load_img('inputs/starry_night.jpg')) * 1.0)[np.newaxis, :]
+print(np.max(img))
+# The layer that contains the "content" understanding of the input images
+content_layers = ["block5_conv2"] 
+
+# The layers that contain the "style" understanding of the input images
+style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+
+content_model = init_model(content_layers)
+content_output = content_model(img)
+print(content_output.shape)
